5792f615bf4fcaac916c1ae94294568fce1006fb
abhi10x10 <abhidev4451ji@gmail.com>
2026-06-09 20:05:02 +0530

    first done

---
+++ Week_2/Day_3/Find_the_Index_of_the_First_Occurrence_in_a_String.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        //Time: O(N) Space:O(1)
+        for(int i=0;i<haystack.size();i++){
+            if(haystack.substr(i,needle.size())==needle) return i;
+        }
+        return -1;
+    }
+};
