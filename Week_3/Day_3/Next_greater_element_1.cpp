ef85f8b436b5f7a51c915c37151817063e331057
abhi10x10 <abhidev4451ji@gmail.com>
2026-06-15 22:06:36 +0530

    third done

---
+++ Week_3/Day_3/Next_greater_element_1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int,int>mp;
+        stack<int>st;
+        
+        int n=nums2.size();
+        st.push(n-1);
+        mp[nums2[n-1]]=-1;
+        for(int i=n-2;i>=0;i--){
+            while(st.size()>0 && nums2[i]>nums2[st.top()]){
+                st.pop();
+            }
+            if(st.size()==0) mp[nums2[i]]=-1;
+            else mp[nums2[i]]=nums2[st.top()];
+            st.push(i);
+        }
+        vector<int>ans;
+        for(int i=0;i<nums1.size();i++){
+            ans.push_back(mp[nums1[i]]);
+        }
+        return ans;
+    }
+};
