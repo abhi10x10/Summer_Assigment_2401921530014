f0405fd15fd390b8d24c6564b9977c92e95a9612
abhi10x10 <abhidev4451ji@gmail.com>
2026-06-16 18:59:07 +0530

    first done

---
+++ Week_3/Day_4/Daily_Temperatures.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& t) {
+        vector<int>ans(t.size(),0);
+        stack<int>st;
+        for(int i=0;i<t.size();i++){
+            while(st.size()>0 && t[st.top()]<t[i]){
+                ans[st.top()]=i-st.top();
+                st.pop();
+            }
+            st.push(i);
+            
+        }
+        return ans;
+    }
+};
