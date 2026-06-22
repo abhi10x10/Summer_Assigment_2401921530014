7d93f5a906b93ea6458d3789aa4759cb3bb9eb48
abhi10x10 <abhidev4451ji@gmail.com>
2026-06-22 23:19:22 +0530

    third done

---
+++ Week_4/Day_4/binary_tree_maximum_path_sum.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int helper(TreeNode* root,int& ans){
+        if(root==NULL) return 0;
+        int left=max(0,helper(root->left,ans));
+        int right=max(0,helper(root->right,ans));
+        //ans=max(ans,max(left+right+root->val,max(left,right)));
+        ans=max(ans,left+right+root->val);
+        return max(left,right)+root->val;
+    }
+    int maxPathSum(TreeNode* root) {
+        if(root==NULL) return 0;
+        int ans=INT_MIN;
+        helper(root,ans);
+        return ans;
+
+    }
+};
